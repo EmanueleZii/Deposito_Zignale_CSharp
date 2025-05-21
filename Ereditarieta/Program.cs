bb57dcd7059612d4a17a0579c7726c78b9955828
Emanuele Zignale <zignale92@gmail.com>
2025-05-21 12:57:34 +0200

    Esempio di ereditarieta

---
+++ Ereditarieta/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Animale
+{
+    public string Nome { get; set; }
+    public int Eta { get; set; }
+
+    public Animale(string nome, int eta)
+    {
+        Nome = nome;
+        Eta = eta;
+    }
+
+    public virtual void Verso()
+    {
+        Console.WriteLine("L'animale fa un verso.");
+    }
+}
+
+public class Cane : Animale
+{
+    public Cane(string nome, int eta) : base(nome, eta)
+    {
+
+    }
+
+    public override void Verso()
+    {
+        Console.WriteLine("Il cane abbaia.");
+    }
+}
