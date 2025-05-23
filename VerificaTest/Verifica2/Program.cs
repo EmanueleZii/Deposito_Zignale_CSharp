cea23adabb1e02908d969eda9fb6a6b122264dbc
Emanuele Zignale <zignale92@gmail.com>
2025-05-23 16:01:26 +0200

    main

---
+++ VerificaTest/Verifica2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class ContoCorrente
+{
+    private decimal saldo;
+    private int numeroOperazione;
+    public decimal Saldo
+    {
+        get
+        {
+            return saldo;
+        }
+        set
+        {
+            saldo = value;
+        }
+
+    }
+
+    public int NumeroOperazione
+    {
+        get
+        {
+            return numeroOperazione;
+        }
+    }
+    public decimal Versa(decimal importo)
+    {
+        return saldo += importo;
+    }
+    public decimal Preleva(decimal prelievo)
+    {
+        return saldo -= prelievo;
+    }
+}
+
+
+public class Program
+{
+    public void Main()
+    {
+        
+    }
+}
