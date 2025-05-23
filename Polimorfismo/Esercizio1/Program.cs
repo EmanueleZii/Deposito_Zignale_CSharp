2e1594e1b8d953d81077996ac21b1c18b1b6bcfd
Emanuele Zignale <zignale92@gmail.com>
2025-05-23 11:45:47 +0200

    extra

--- Polimorfismo/Esercizio1/Program.cs
+++ Polimorfismo/Esercizio1/Program.cs
@@ -3,9 +3,31 @@ using System.Collections.Generic;
     class Veicolo
     {
         public string Targa { get; set; }
+        private int riparazioni;
+        public int Riparazioni
+        {
+            get { return riparazioni; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Il numero di riparazioni non può essere negativo.");
+                    riparazioni = 0;
+                }
+                if (value >= 3)
+                {
+                    riparazioni = 3;
+                }
+                else
+                {
+                    riparazioni = value;
+                }
+            }
+        }
+
         public virtual void Ripara()
-        { 
-                Console.WriteLine("Il veicolo viene controllato.");
+        {
+            Console.WriteLine("Il veicolo viene controllato.");
         }
     }
     class Auto : Veicolo
@@ -24,7 +46,7 @@ using System.Collections.Generic;
             Console.WriteLine("Controllo catena, freni e motore della moto.");
         }
     }
-    class Furgone : Veicolo
+    class Camion : Veicolo
     {
         public override void Ripara()
         {
@@ -32,60 +54,156 @@ using System.Collections.Generic;
             Console.WriteLine("Controllo sospensione, freni rinforzati e carico del camion.");
         }
     }
-    class Program {
-        
-        static void Main() {
-            List<Veicolo> veicoli = new List<Veicolo>
+class Program
+{
+    static void Main()
+    {
+        // Creazione di un oggetto di tipo Veicolo
+        List<Veicolo> veicoli = new List<Veicolo>
                     {
                         new Auto(),
                         new Moto(),
                         new Furgone()
                     };
-            Console.WriteLine("Benvenuto nel programma di controllo veicoli.");
-            Console.WriteLine("Inserisci la targa del veicolo:");
-            foreach (var veicolo in veicoli)
-            {
-                Console.Write("Targa: ");
-                veicolo.Targa = Console.ReadLine();
-            }
-            bool continua = true;
+        // Impostazione della targa del veicolo
+        Console.WriteLine("Benvenuto nel programma di controllo veicoli.");
+        Console.WriteLine("Inserisci la targa del veicolo:");
+        foreach (var veicolo in veicoli)
+        {
+            Console.Write("Targa: ");
+            veicolo.Targa = Console.ReadLine();
+        }
+        // Impostazione del numero di riparazioni
 
-            while (continua)
-            {
-                Console.WriteLine("Scegli un veicolo da controllare:");
-                Console.WriteLine("1. Auto");
-                Console.WriteLine("2. Moto");
-                Console.WriteLine("3. Furgone");
-                Console.WriteLine("4. Controlla tutti i veicoli");
-                Console.WriteLine("5. Esci");
-                string scelta = Console.ReadLine();
+        bool continua = true;
+        Moto MotoRiparata = new Moto();
+        MotoRiparata.Riparazioni = 0;
+        Auto AutoRiparata = new Auto();
+        AutoRiparata.Riparazioni = 0;
+        Camion FurgoneRiparato = new Camion();
+        FurgoneRiparato.Riparazioni = 0;
+        int budget = 0;
+        Console.WriteLine("Inserisci il budget per le riparazioni:");
+        budget = int.Parse(Console.ReadLine());
 
-                switch (scelta)
-                {
-                    case "1":
-                        veicoli[0].Ripara();
-                        break;
-                    case "2":
-                        veicoli[1].Ripara();
-                        break;
-                    case "3":
-                        veicoli[2].Ripara();
-                        break;
-                    case "4":
-                        foreach (var veicolo in veicoli)
+        // Ciclo di controllo dei veicoli
+        while (continua)
+        {
+            Menu();
+            Console.WriteLine("Scegli un'opzione:");
+            string scelta = Console.ReadLine();
+            // Controllo del numero di riparazioni
+            switch (scelta)
+            {
+                case "1":
+                    veicoli[0].Ripara();
+                    AutoRiparata.Riparazioni++;
+                    
+                    if (AutoRiparata.Riparazioni >= 3 )
+                    {
+                        Console.WriteLine("Il veicolo non può essere riparato.");
+                        AutoRiparata.Riparazioni = 0;
+                    }
+                    else if (budget < 100)
+                    {
+                        Console.WriteLine("Il budget non è sufficiente per la riparazione.");
+                    }
+                    else
+                    {
+                        budget -= 100;
+                        Console.WriteLine($"Il veicolo è stato riparato {AutoRiparata.Riparazioni} volte.");
+                    }
+                    break;
+                case "2":
+                    // Controllo del numero di riparazioni
+                    veicoli[1].Ripara();
+                    MotoRiparata.Riparazioni++;
+
+                    if (MotoRiparata.Riparazioni >= 3)
+                    {
+                        Console.WriteLine("Il veicolo non può essere riparato.");
+                        MotoRiparata.Riparazioni = 0;
+
+                    }
+                    else if (budget < 100)
+                    {
+                        Console.WriteLine("Il budget non è sufficiente per la riparazione.");
+                    }
+                    else
+                    {
+                        budget -= 100;
+                        Console.WriteLine($"Il veicolo è stato riparato {MotoRiparata.Riparazioni} volte.");
+                    }
+                    
+                    break;
+                case "3":
+                    veicoli[2].Ripara();
+                    FurgoneRiparato.Riparazioni++;
+                    if (veicoli[2].Riparazioni >= 3&& budget > 100)
+                    {
+                        Console.WriteLine("Il veicolo non può essere riparato.");
+                        FurgoneRiparato.Riparazioni = 0;
+                    }
+                    else if (budget < 100)
+                    {
+                        Console.WriteLine("Il budget non è sufficiente per la riparazione.");
+                    }
+                    else
+                    {
+                        budget -= 100;
+                        Console.WriteLine($"Il veicolo è stato riparato {FurgoneRiparato.Riparazioni} volte.");
+                    }
+                    break;
+                case "4":
+                    foreach (var veicolo in veicoli)
+                    {
+                        veicolo.Ripara();
+                        if (AutoRiparata.Riparazioni >= 3)
                         {
-                            veicolo.Ripara();
+                            Console.WriteLine("Il veicolo non può essere riparato.");
+                            AutoRiparata.Riparazioni = 0;
                         }
-                        Console.WriteLine("Tutti i veicoli sono stati controllati.");
-                        break;
-                    case "5":
-                        continua = false;
-                        Console.WriteLine("Uscita dal programma.");
-                        break;
-                    default:
-                        Console.WriteLine("Scelta non valida.");
-                        break;
-                }
+                        else if (MotoRiparata.Riparazioni >= 3)
+                        {
+                            Console.WriteLine("Il veicolo non può essere riparato.");
+                            MotoRiparata.Riparazioni = 0;
+                        }
+                        else if (FurgoneRiparato.Riparazioni >= 3)
+                        {
+                            Console.WriteLine("Il veicolo non può essere riparato.");
+                            FurgoneRiparato.Riparazioni = 0;
+                        }
+                        else if (budget < 100)
+                        {
+                            Console.WriteLine("Il budget non è sufficiente per la riparazione.");
+                        }
+                        else
+                        {
+                            budget -= 300;
+                            Console.WriteLine($"Il veicolo è stato riparato {veicolo.Riparazioni} volte.");
+                        }
+
+                    }
+                    Console.WriteLine("Tutti i veicoli sono stati controllati.");
+                    break;
+                case "5":
+                    continua = false;
+                    Console.WriteLine("Uscita dal programma.");
+                    break;
+                default:
+                    Console.WriteLine("Scelta non valida.");
+                    break;
             }
         }
-}
+    }
+    public static void Menu()
+    {
+        Console.WriteLine("Benvenuto nel programma di controllo veicoli.");
+        Console.WriteLine("1. Auto");
+        Console.WriteLine("2. Moto");
+        Console.WriteLine("3. Furgone");
+        Console.WriteLine("4. Controlla tutti i veicoli");
+        Console.WriteLine("5. Esci");
+    }
+
+} 
