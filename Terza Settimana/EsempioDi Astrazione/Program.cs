318521c1cdef8c3f2a27449886e60450bdda54f0
Emanuele Zignale <zignale92@gmail.com>
2025-05-26 11:16:56 +0200

    esempio di astrazione

---
+++ Terza Settimana/EsempioDi Astrazione/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+//classe Astratta
+public abstract class Veicolo
+{
+    public abstract void Avvia();
+}
+
+//classe concreta che estende la classe Astratta
+public class Auto : Veicolo
+{
+    public override void Avvia()
+    {
+        Console.WriteLine("L'auto è stata avviata.");
+    }
+}
+//interfaccia
+public interface IVeicoloElettrico
+{
+    void Ricarica();
+}
+
+//classe che implementa l'interfaccia
+public class ScooterElettrica : Auto, IVeicoloElettrico
+{
+    public override void Avvia()
+    {
+        Console.WriteLine("L'auto elettrica è stata avviata.");
+    }
+
+    public void Ricarica()
+    {
+        Console.WriteLine("L'auto elettrica è in ricarica.");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Veicolo miaAuto = new Auto();
+        miaAuto.Avvia();
+
+        ScooterElettrica miaScooterElettrica = new ScooterElettrica();
+        miaScooterElettrica.Avvia();
+        miaScooterElettrica.Ricarica();
+    }
+}
