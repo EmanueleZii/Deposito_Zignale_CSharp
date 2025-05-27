c21c5450a304187fb7ced734a8878758cfb6269b
Emanuele Zignale <zignale92@gmail.com>
2025-05-27 08:58:40 +0200

    EsercizioDiGruppo3

---
+++ Esercizi Di Gruupo/Progetto3_Gruppo_AAE_C-/Emanuele/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class Corso
+{
+    public string Titolo { get; set; }
+    private int DurataOre { get; set; }
+    
+    public int DurataOre
+    {
+        get { return durataOre; }
+        set {
+            if (value >= 0)
+                durataOre = value;
+        }
+    }
+
+    public Corso()
+    {
+        Titolo = "";
+        DurataOre = 0;
+    }
+
+    public abstract void ErogaCorso();
+    public abstract void StampaDettagli();
+}
+
+public class CorsoOnline : Corso {
+    private string Piattaforma { get; set; }
+    private string LinkAccesso { get; set; }
+    
+    public string piattaforma { get { return Piattaforma; } set { Piattaforma = value; } }
+
+    public string linkAccesso { get { return LinkAccesso; } set { LinkAccesso = value; } }
+
+    public CorsoOnline(string titolo, int durataOre, string _piattaforma, string _linkAccesso) : base(titolo, durataOre)
+    {
+        Piattaforma = _piattaforma;
+        LinkAccesso = _linkAccesso;
+    }
+    public override void StampaDettagli()
+    {
+        Console.WriteLine($"Accesso al corso online: {linkAccesso} su {Piattaforma}");   
+    }
+    public override void ErrogaCorso()
+    {
+        Console.WriteLine($"Il corso online {piattaforma} è stato erogato con successo.");
+    }
+
+    public override string ToString()
+    {
+        return $"Accesso al corso online: {linkAccesso} su {piattaforma}" +
+               $"\nDurata: {DurataOre} ore" +
+               $"\nLink di accesso: {LinkAccesso}";
+    }
+}
