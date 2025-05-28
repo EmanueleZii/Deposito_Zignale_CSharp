e53c39936bcf52687ca1cfbd15e8bba1c88929a3
Emanuele Zignale <zignale92@gmail.com>
2025-05-28 14:46:21 +0200

    esercizio 2 factorymethod

---
+++ Terza Settimana/DesignPattern/FactoryMethod/Esercizio2FactoryMethod  copia/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+// Design Pattern Factory Method
+
+// 1. Iveicolo: definisce l'interfaccia del prodotto
+public interface IShape {
+    void Draw();
+}
+// 2. Auto: implementa IProduct
+public class Circle : IShape {
+    public void Draw()
+    {
+        Console.WriteLine("Circle creato");
+    }
+}
+// 3. Moto: un altro prodotto concreto
+public class Square : IShape {
+    public void Draw()
+    {
+        Console.WriteLine("Square creato");
+    }
+}
+public abstract class ShapeCreator
+{
+    public abstract IShape CreateShape(string type);
+}
+
+// 4. Creator: dichiara VeicoloFactory
+public class ConcreteShapeCreator : ShapeCreator {
+    public override IShape CreateShape(string tipo)
+    {
+        switch (tipo.ToLower())
+        {
+            case "auto":
+                return new Circle();
+                break;
+            case "moto":
+                return new Square();
+                break;
+            default:
+                Console.Write("scelta non valida");
+                return null;
+                break;
+        }
+    }
+}
+class Program {
+    static void Main()
+    {
+
+        Console.WriteLine("Quale Forma vuoi disegnare");
+        string input = Console.ReadLine();
+        ShapeCreator create = new ConcreteShapeCreator();
+
+        IShape disegna = create.CreateShape(input);
+        disegna.Draw();
+
+    }
+}
